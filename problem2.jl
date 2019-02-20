8bfc701d04209231d0e04de1738834e4dbedbb9f
Samuel <swiqvist@gmail.com>
2019-02-20 15:05:13 +0100

    almost done with problem 1, just need to fix the plots a bit

--- problem2.jl
+++ problem2.jl
@@ -1,5 +1,11 @@
-# load packages 
+# load packages
 using Optim
+using LinearAlgebra
+#using NLSolversBase
+using Distributions
+using ForwardDiff
+using KernelDensity
+using PyPlot
 
 # load file
 file = open("eBayNumberOfBidderData.dat")
@@ -26,18 +32,38 @@ end
 #rosenbrock(x) =  (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2
 #result = optimize(rosenbrock, zeros(2), BFGS())
 
+# set model parameters
+
+τ_0 = 10
+prior_cov_m = τ_0^2*Matrix{Float64}(I, nbr_covariates, nbr_covariates)
+prior_cov_m_inv = inv(prior_cov_m)
+
+dist_marginal_prior = Normal(0, τ_0)
 # optmization problem
 
 # logposterior function
 function logposterior(β)
 
+    logpos = 0.
 
+    for i = 1:nbr_obs
 
+        logpos = logpos + ((X[:,i]'*β)*y[i] - exp(X[:,i]'*β))
+
+    end
+
+    logpos = logpos - 0.5*β'prior_cov_m_inv*β
+
+    return -logpos # Optim does minimization, hence the minus sign
 
 end
 
 # start values for optimizer
+
 β_start = ones(nbr_covariates)
+
+# create a twice differentable fuction (to get the Hessian after the optmization
+# see http://julianlsolvers.github.io/Optim.jl/stable/#examples/generated/maxlikenlm/#maximum-likelihood-estimation-the-normal-linear-model)
 
 # run optimization using conjugate gradient decent with numerical gradients
 opt = optimize(logposterior, β_start, ConjugateGradient())
@@ -45,10 +71,86 @@ opt = optimize(logposterior, β_start, ConjugateGradient())
 # get parameter estimations (posterior mean)
 β_tilde = Optim.minimizer(opt)
 
-# get Hessian matrix
-numerical_hessian = hessian!(logposterior,β_tilde)
+numerical_hessian = Symmetric(ForwardDiff.hessian(logposterior, β_tilde))
 
 # sample from approx posterior
+posterior_mean = β_tilde
+
+posterior_cov_m = inv(-numerical_hessian) + 10^(-2)*Matrix{Float64}(I, nbr_covariates, nbr_covariates)
+
+
+approx_posterior = MvNormal(posterior_mean, posterior_cov_m)
+
+posterior_samples = rand(approx_posterior, 10^4)
 
 
 # plot results
+
+h1_β1 = kde(posterior_samples[1,:])
+h1_β2 = kde(posterior_samples[2,:])
+h1_β3 = kde(posterior_samples[3,:])
+h1_β4 = kde(posterior_samples[4,:])
+h1_β5 = kde(posterior_samples[5,:])
+h1_β6 = kde(posterior_samples[6,:])
+h1_β7 = kde(posterior_samples[7,:])
+h1_β8 = kde(posterior_samples[8,:])
+h1_β9 = kde(posterior_samples[9,:])
+
+println("Posterior means:")
+print(mean(posterior_samples,dims = 2))
+
+println("Posterior std:")
+print(std(posterior_samples,dims = 2))
+
+PyPlot.figure()
+PyPlot.subplot(331)
+PyPlot.plot(h1_β1.x,h1_β1.density, "b")
+PyPlot.plot(h1_β1.x,pdf.(dist_marginal_prior, h1_β1.x), "g")
+PyPlot.subplot(332)
+PyPlot.plot(h1_β2.x,h1_β2.density, "b")
+PyPlot.plot(h1_β2.x,pdf.(dist_marginal_prior, h1_β2.x), "g")
+PyPlot.subplot(333)
+PyPlot.plot(h1_β3.x,h1_β3.density, "b")
+PyPlot.plot(h1_β3.x,pdf.(dist_marginal_prior, h1_β3.x), "g")
+PyPlot.subplot(334)
+PyPlot.plot(h1_β4.x,h1_β4.density, "b")
+PyPlot.plot(h1_β4.x,pdf.(dist_marginal_prior, h1_β4.x), "g")
+PyPlot.subplot(335)
+PyPlot.plot(h1_β5.x,h1_β5.density, "b")
+PyPlot.plot(h1_β5.x,pdf.(dist_marginal_prior, h1_β5.x), "g")
+PyPlot.subplot(336)
+PyPlot.plot(h1_β6.x,h1_β6.density, "b")
+PyPlot.plot(h1_β6.x,pdf.(dist_marginal_prior, h1_β6.x), "g")
+PyPlot.subplot(337)
+PyPlot.plot(h1_β7.x,h1_β7.density, "b")
+PyPlot.plot(h1_β7.x,pdf.(dist_marginal_prior, h1_β7.x), "g")
+PyPlot.subplot(338)
+PyPlot.plot(h1_β8.x,h1_β8.density, "b")
+PyPlot.plot(h1_β8.x,pdf.(dist_marginal_prior, h1_β8.x), "g")
+PyPlot.subplot(339)
+PyPlot.plot(h1_β9.x,h1_β9.density, "b")
+PyPlot.plot(h1_β9.x,pdf.(dist_marginal_prior, h1_β9.x), "g")
+
+
+# simulate from posterior predictive
+
+posterior_samples = rand(approx_posterior, 10^3)
+posterior_pred_bids = zeros(10^3)
+
+x_case = [1, 1, 1, 1, 0, 0, 0, 1, 0.5]
+
+for i = 1:10^3
+    dist_bids = Poisson(exp(x_case'*posterior_samples[:,i]))
+    posterior_pred_bids[i] = rand(dist_bids)
+end
+
+println("Posterior means:")
+print(mean(posterior_pred_bids))
+
+println("Posterior std:")
+print(std(posterior_pred_bids))
+
+PyPlot.figure()
+h = PyPlot.plt[:hist](posterior_pred_bids,10)
+
+# here we should have a bar plot
